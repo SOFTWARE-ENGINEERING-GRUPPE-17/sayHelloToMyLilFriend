fe060886c769057ecca7b6b4c3adb3edbfcc35ae
Prebenww <63924500+Prebenww@users.noreply.github.com>
2020-09-26 15:22:25 +0200

    Oppdatert frontend kode
    React frontend.

---
+++ src/screens/Rent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+
+function Rent() {
+    return (
+        <div>
+            <h1>Lei ut parkering</h1>
+        </div>
+    );
+}
+
+export default Rent;
