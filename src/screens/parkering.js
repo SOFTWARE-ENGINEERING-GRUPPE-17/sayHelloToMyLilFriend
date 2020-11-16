b7b907470a10398898ee25dae8deb3cae99cbf24
Preben <suprosaX123>
2020-11-16 19:22:08 +0100

    implementert dato funksjonalitet

--- src/screens/parkering.js
+++ src/screens/parkering.js
@@ -49,11 +49,19 @@ function Park() {
             <Card style={styles}>
                 <Card.Img width={400} height={250} variant="top"
                           src="https://cdn.homedit.com/wp-content/uploads/2017/04/Tree-House-open-space-Garage-for-two-cars-1024x682.jpg"/>
-                <Card.Body>
+                <Card.Body  style={{backgroundColor: "#ededed", borderRadius: 25, padding: 10}}>
                     <p>Fra:</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <p>Til</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <Card.Title>Langkaia 1
                         0150 Oslo</Card.Title>
 
@@ -68,11 +76,19 @@ function Park() {
             <Card style={styles2}>
                 <Card.Img width={400} height={250} variant="top"
                           src="https://i.pinimg.com/originals/9d/bf/54/9dbf54c3e738cd5ee5a5cab5209d1e9d.jpg"/>
-                <Card.Body>
+                <Card.Body style={{backgroundColor: "#ededed", borderRadius: 25, padding: 10}}>
                     <p>Fra:</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <p>Til</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <Card.Title>Biskop Gunnerus' gate 14A
                         0185 Oslo</Card.Title>
                     <Card.Text>
@@ -87,9 +103,17 @@ function Park() {
                           src="https://mygate.com/blog/wp-content/uploads/2020/03/parking_blogbanner-22.jpg"/>
                 <Card.Body>
                     <p>Fra:</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <p>Til</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <Card.Title>Haakon VIIs gate 9
                         0161 Oslo</Card.Title>
                     <Card.Text>
@@ -102,11 +126,19 @@ function Park() {
             <Card style={styles2}>
                 <Card.Img width={400} height={250} variant="top"
                           src="https://www.autoguide.com/auto-news/wp-content/uploads//2012/01/parkatmyhouse.jpg"/>
-                <Card.Body>
+                <Card.Body style={{backgroundColor: "#ededed", borderRadius: 25, padding: 10}}>
                     <p>Fra:</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <p>Til</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <Card.Title>Smalvollveien 44
                         0667 Oslo</Card.Title>
                     <Card.Text>
@@ -121,9 +153,17 @@ function Park() {
                           src="https://www.toronto.ca/wp-content/uploads/2017/11/9929-applying-for-a-permit.jpg"/>
                 <Card.Body>
                     <p>Fra:</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <p>Til</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <Card.Title>Ole Deviks vei 24
                         0666 Oslo</Card.Title>
                     <Card.Text>
@@ -136,11 +176,19 @@ function Park() {
             <Card style={styles2}>
                 <Card.Img width={400} height={250} variant="top"
                           src="https://live.staticflickr.com/4124/5103266193_afd3ba7258_n.jpg"/>
-                <Card.Body>
+                <Card.Body style={{backgroundColor: "#ededed", borderRadius: 25, padding: 10}}>
                     <p>Fra:</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <p>Til</p>
-                    <DatePicker/>
+                    <DatePicker
+                        yearPlaceholder={"2020"}
+                        monthPlaceholder={"01"}
+                        dayPlaceholder={"01"}
+                    />
                     <Card.Title>Brobekkveien 80
                         0582 Oslo</Card.Title>
                     <Card.Text>
