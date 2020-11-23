7270a44ca074eafbfb58b1fdf2b156d94ed634a6
dennisolsen <denniso@hiof.no>
2020-11-23 16:27:23 +0100

    "Register form exists" er nesten ferdig, men må debugges

--- src/screens/Rent..test.js
+++ src/screens/Rent..test.js
@@ -4,6 +4,9 @@ import Adapter from 'enzyme-adapter-react-16'
 
 import Rent from "./Rent";
 import Home from "./HomeScreen";
+import Park from "./Park";
+import {ItemCard} from "../Components/ItemCard";
+import {AddItem} from "../Components/AddItem";
 
 describe('Rent Component', () => {
 
@@ -15,4 +18,15 @@ describe('Rent Component', () => {
     const component = shallow(<Rent/>)
     expect(component).toMatchSnapshot()
   })
+
+  it("Register form exists", () => {
+    const wrapper = shallow(<Park/>);
+    const formHeader = <h1 className="display-4 my-4 text-center text-muted">Parkeringer</h1>;
+    expect(wrapper.contains(formHeader)).toEqual(true);
+    expect(wrapper.contains(AddItem)).toEqual(true);
+    expect(wrapper.contains(ItemCard)).toEqual(true);
+    // expect(wrapper.find("Card.Card1").text());
+
+  });
+
 })
